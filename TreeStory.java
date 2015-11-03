7b85bbb257940213e68bf7d8b73aabfb0ca82c15
amykangweb <selene6023@gmail.com>
2015-11-02 17:37:29 -0800

    Quit program if user enters dork for noun.

--- TreeStory.java
+++ TreeStory.java
@@ -4,7 +4,8 @@ public class TreeStory {
 
   public static void main(String[] args) {
     Console console = System.console();
-    int age = 12;
+    String ageAsString = console.readLine("How old are you? ");
+    int age = Integer.parseInt(ageAsString);
     if (age < 13) {
       console.printf("Sorry you must be at least 13 to use this program.\n");
       System.exit(0);
@@ -12,6 +13,10 @@ public class TreeStory {
     String name = console.readLine("Enter your name: ");
     String adjective = console.readLine("Enter an adjective: ");
     String noun = console.readLine("Enter a noun: ");
+    if (noun.equalsIgnoreCase("dork")) {
+      console.printf("That language is not allowed. Exiting. \n\n");
+      System.exit(0);
+    }
     String adverb = console.readLine("Enter an adverb: ");
     String verb = console.readLine("Enter a verb ending in -ing: ");
 
